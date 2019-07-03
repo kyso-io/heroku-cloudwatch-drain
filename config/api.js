cda50169c9d73333bb92d623127b293474a22d33
eoinmurray <amonecho1@gmail.com>
2019-07-03 12:12:42 +0200

    setup to be easy

---
+++ config/api.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // how many messages should we buffer and send to AWS at once, do not change unless necessary
+  batchSize: 50,
+
+  // at what port should our server be listening
+  serverPort: 8000,
+
+  // your cloudwatch log group
+  logGroup: "heroku",
+
+  // prefix for all stream names (a random number will be appended)
+  logStreamPrefix: "api",
+
+  // we strongly recommend using env variables for the credentials, however you can still add them here
+  awsCredentials: {
+    region: "us-east-1",
+  },
+
+  // access token, which will be sent from heroku on every request
+  accessToken: process.env.HEROKU_TOKEN
+};
---
+++ config/ui.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // how many messages should we buffer and send to AWS at once, do not change unless necessary
+  batchSize: 50,
+
+  // at what port should our server be listening
+  serverPort: 8001,
+
+  // your cloudwatch log group
+  logGroup: "heroku",
+
+  // prefix for all stream names (a random number will be appended)
+  logStreamPrefix: "ui",
+
+  // we strongly recommend using env variables for the credentials, however you can still add them here
+  awsCredentials: {
+    region: "us-east-1",
+  },
+
+  // access token, which will be sent from heroku on every request
+  accessToken: process.env.HEROKU_TOKEN
+};
